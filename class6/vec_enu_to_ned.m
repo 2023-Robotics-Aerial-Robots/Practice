ab52a781451305865b3bce8d07bc071ad6b7cb7f
chun-chih <108335724+chun-chih@users.noreply.github.com>
2024-04-14 11:47:11 +0800

    Update vec_enu_to_ned.m

--- class6/vec_enu_to_ned.m
+++ class6/vec_enu_to_ned.m
@@ -1,6 +1,7 @@
 function ned = vec_enu_to_ned(enu)
     ned = zeros(3, 1);
-    ned(1) = enu(2);
-    ned(2) = enu(1);
-    ned(3) = -enu(3);
+%   -------- checkpoint1-----------
+    ned(1) = ;
+    ned(2) = ;
+    ned(3) = ;
 end
